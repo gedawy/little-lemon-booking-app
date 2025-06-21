27667d14ff7fc9f2c4c3bc0a603cfe03fe87e750
gedawy <omargedawy@outlook.com>
2025-06-22 02:30:39 +0300

    chore: remove console.log

--- src/pages/Booking/Booking.js
+++ src/pages/Booking/Booking.js
@@ -2,7 +2,6 @@ import BookingForm from "../../components/BookingForm/BookingForm";
 import "./Booking.css";
 
 const Bookings = ({ availableTimes, dispatchAvailableTimes, submitForm }) => {
-  console.log("availableTimes ===> ", availableTimes);
   return (
     <section className="booking">
       <h1 className="booking__title">Table Reservation</h1>
